c59911c696c4e9e2553573d95720f6105a60eadf
Sebastian Porto <sebasporto@gmail.com>
2016-10-08 20:50:15 +1100

    Finish bot.ml Ocaml

--- ocaml/bob/bob.ml
+++ ocaml/bob/bob.ml
@@ -1,19 +1,22 @@
+open Str
+
 let isQuestion q =
-	String.contains_from q (String.length q) '?';;
+	Str.string_match (Str.regexp ".+\?$") q 0
+
+let isUpper q =
+	String.equal (String.uppercase_ascii q) q
+
+let hasLetters q =
+	Str.string_match (Str.regexp ".+[a-zA-Z]+") q 0 ;;
 
 let isYell q =
-	let isSame =
-		String.uppercase q == q
-	in let hasLetters =
-		
-	in
-		hasLetters && isSame
+	hasLetters q && isUpper q
 
 let response_for q =
 	let trimmed =
 		String.trim q
 	in
-		if trimmed == "" then
+		if String.equal trimmed "" then
 			"Fine. Be that way!"
 		else if isYell trimmed then
 			"Whoa, chill out!" 
