0171398608ce9d4bd213cb440a086ecf620ca6d5
Sebastian Porto <sebasporto@gmail.com>
2016-10-21 18:56:12 +1100

    Refine AT cypher

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,42 +1,25 @@
 use std::ascii::AsciiExt;
 
-static PLAIN: &'static str = "abcdefghijklmnopqrstuvwxyz";
-static CYPHER: &'static str = "zyxwvutsrqponmlkjihgfedcba";
-
-fn switch(c: char, source: &str, target: &str) -> char {
-    match source.find(c) {
-        Some(ix) => {
-            match target.chars().nth(ix) {
-                Some(targetChar) => targetChar,
-                None => c,
-            }
-        }
-        None => c,
+fn switch(c: char) -> char {
+    if c.is_numeric() {
+        c
+    } else {
+        ('z' as u8 + 'a' as u8 - c as u8) as char
     }
-}
-
-fn decodeChar(c: char) -> char {
-    switch(c, CYPHER, PLAIN)
-}
-
-fn encodeChar(c: char) -> char {
-    switch(c, PLAIN, CYPHER)
 }
 
 pub fn decode(input: &str) -> String {
     input.chars()
         .filter(|c| !c.is_whitespace())
-        .map(decodeChar)
+        .map(switch)
         .collect()
 }
 
 pub fn encode(input: &str) -> String {
     input.to_lowercase()
         .chars()
-        .filter(|c| !c.is_whitespace())
-        .filter(|c| c.is_alphanumeric())
-        .filter(|c| c.is_ascii())
-        .map(encodeChar)
+        .filter(|c| c.is_alphanumeric() && c.is_ascii())
+        .map(switch)
         .collect::<Vec<char>>()
         .chunks(5)
         .map(|x| x.iter().cloned().collect())
