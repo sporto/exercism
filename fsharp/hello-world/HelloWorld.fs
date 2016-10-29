a1ac1e27a0da33b1c71461999173ac37ed78ee09
Sebastian Porto <sebasporto@gmail.com>
2016-10-29 23:54:09 +1100

    Add F# Hello World

---
+++ fsharp/hello-world/HelloWorld.fs
@@ -0,0 +1,7 @@
+﻿// Learn more about F# at http://fsharp.org
+module HelloWorld
+
+let hello input = 
+    match input with
+    | None -> "Hello, World!"
+    | Some name -> "Hello, " + name + "!"
--- fsharp/hello-world/HelloWorldTest.fs
+++ fsharp/hello-world/HelloWorldTest.fs
@@ -9,11 +9,9 @@ let ``No name`` () =
     Assert.That(hello None, Is.EqualTo("Hello, World!"))
 
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Sample name`` () =
     Assert.That(hello (Some "Alice"), Is.EqualTo("Hello, Alice!"))
 
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Other sample name`` () =
     Assert.That(hello (Some "Bob"), Is.EqualTo("Hello, Bob!"))
