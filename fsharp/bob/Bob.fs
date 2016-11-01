ed6b2ec0508cbd0e0188c3c4585ce3db52b89ec2
Sebastian Porto <sebasporto@gmail.com>
2016-11-02 08:22:20 +1100

    Add Bob Fsharp

---
+++ fsharp/bob/Bob.fs
@@ -0,0 +1,21 @@
+﻿module Bob
+
+open System.Text.RegularExpressions
+
+let isYell input =
+    Regex.IsMatch(input, @"[A-Za-z]+") 
+    && input.ToUpper() = input
+
+let isQuestion input =
+    Regex.IsMatch(input, @".+\?\z")
+
+let hey (input:string) =
+    let trimmed = input.Trim()
+    match trimmed with
+    | t when t = "" -> "Fine. Be that way!"
+    | t when isYell t -> "Whoa, chill out!"
+    | t when isQuestion t -> "Sure."
+    | _ -> "Whatever."
+
+
+
