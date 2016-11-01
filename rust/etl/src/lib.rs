cbb6796170718319dcbf7f864e2b12314171c070
Sebastian Porto <sebasporto@gmail.com>
2016-11-02 08:29:15 +1100

    Add ETL in Rust

---
+++ rust/etl/src/lib.rs
@@ -0,0 +1,13 @@
+const scores = [
+    ("AEIOULNRST", 1),
+    ("DG", 2),
+    ("BCMP", 3),
+    ("FHVWY", 4),
+    ("K", 5),
+    ("JX", 8),
+    ("QZ", 10)
+]
+
+pub fn transform(BTreeMap<i32, Vec<String>>) -> BTreeMap<String, i32> {
+    
+}
