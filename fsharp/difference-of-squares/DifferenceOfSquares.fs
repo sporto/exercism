1315c7d137786b01b43799f77fa518b5ed4fa972
Sebastian Porto <sebasporto@gmail.com>
2016-11-02 21:18:49 +1100

    Add DifferenceOfSquares in F#

---
+++ fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,17 @@
+﻿module DifferenceOfSquares
+
+let square n =
+    pown n 2
+
+let sumOfSquares n =
+    [1..n]
+        |> List.map square
+        |> List.sum
+
+let squareOfSums n =
+    [1..n]
+        |> List.sum
+        |> square
+
+let difference n = 
+    squareOfSums n - sumOfSquares n
