e4bb51775d19e76bed582f28a28c6ae506f0fb1f
Sebastian Porto <sebasporto@gmail.com>
2016-11-02 21:04:36 +1100

    Add Leap in F#

---
+++ fsharp/leap/LeapTest.fs
@@ -0,0 +1,20 @@
+﻿module LeapYearTest
+
+open NUnit.Framework
+open LeapYear
+    
+[<Test>]
+let ``Is 1996 a valid leap year`` () = 
+    Assert.That(isLeapYear 1996, Is.True)
+    
+[<Test>]
+let ``Is 1997 an invalid leap year`` () = 
+    Assert.That(isLeapYear 1997, Is.False)
+    
+[<Test>]
+let ``Is the turn of the 20th century an invalid leap year`` () = 
+    Assert.That(isLeapYear 1900, Is.False)
+    
+[<Test>]
+let ``Is the turn of the 25th century a valid leap year`` () = 
+    Assert.That(isLeapYear 2400, Is.True)
---
+++ fsharp/leap/LeapYear.fs
@@ -0,0 +1,10 @@
+﻿module LeapYear
+
+let isLeapYear year =
+    let by4 = year % 4 = 0
+    let by100 = year % 100 = 0
+    let by400 = year % 400 = 0
+    match year with
+    | _ when by4 && by100 -> by400
+    | _ when by4 -> true
+    | _ -> false
