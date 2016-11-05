18e2ceed369a6357cf8a43ab2d53df832262114b
Sebastian Porto <sebasporto@gmail.com>
2016-11-05 20:09:05 +1100

    Add SumOfMultiples.fs

---
+++ fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -0,0 +1,11 @@
+﻿module SumOfMultiples
+
+let multiplesOf max n =
+    [n..n..(max - 1)]
+
+let sumOfMultiples numbers max =
+    numbers
+        |> Seq.map (multiplesOf max)
+        |> Seq.concat
+        |> Seq.distinct
+        |> Seq.sum
