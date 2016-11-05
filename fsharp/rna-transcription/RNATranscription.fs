b1450afe541fe2685917ac44430f7d709036662f
Sebastian Porto <sebasporto@gmail.com>
2016-11-05 15:49:15 +1100

    Finish RNATranscription.fs F#

---
+++ fsharp/rna-transcription/RNATranscription.fs
@@ -0,0 +1,12 @@
+﻿module RNATranscription
+
+let chartToRNA char =
+    match char with
+    | 'G' -> 'C'
+    | 'C' -> 'G'
+    | 'T' -> 'A'
+    | 'A' -> 'U'
+    | _ -> char
+
+let toRna seq =
+    seq |> String.map chartToRNA
---
+++ fsharp/rna-transcription/RNATranscriptionTest.fs
@@ -0,0 +1,25 @@
+﻿module RNATranscriptionTest
+
+open NUnit.Framework
+
+open RNATranscription
+    
+[<Test>]
+let ``Rna complement of cytosine is guanine`` () =
+    Assert.That(toRna "C", Is.EqualTo("G"))
+
+[<Test>]
+let ``Rna complement of guanine is cytosine`` () =
+    Assert.That(toRna "G", Is.EqualTo("C"))
+
+[<Test>]
+let ``Rna complement of thymine is adenine`` () =
+    Assert.That(toRna "T", Is.EqualTo("A"))
+
+[<Test>]
+let ``Rna complement of adenine is uracil`` () =
+    Assert.That(toRna "A", Is.EqualTo("U"))
+
+[<Test>]
+let ``Rna complement`` () =
+    Assert.That(toRna "ACGTGGTCTTAA", Is.EqualTo("UGCACCAGAAUU"))
