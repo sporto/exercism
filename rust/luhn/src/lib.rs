0e2cc21aad0e559736ec5d9e8cdc5ae2689b79da
Sebastian <s@porto5.com>
2017-07-25 19:13:17 +1000

    Modify Rust Luhn

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,15 +1,48 @@
+fn remove_whitespace(input: &str) -> Result<String, String> {
+    let trimmed = input.replace(" ", "");
+
+    if trimmed.len() < 2 {
+        Err("Invalid".to_string())
+    } else {
+        Ok(trimmed)
+    }
+}
+
+fn to_digits(input: String) -> Result<Vec<u32>, String> {
+    input
+        .chars()
+        .map(|c| c.to_digit(10))
+        .collect::<Option<Vec<u32>>>()
+        .ok_or("Invalid digits".to_string())
+}
+
+fn double_second_digits(digits: Vec<u32>) -> Vec<u32> {
+    digits
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(ix, &num)|
+            if ix % 2 == 0 { num } else { num * 2 }
+        )
+        .map(|num|
+            if num > 9 { num - 9 } else { num }
+        )
+        .collect::<Vec<u32>>()
+}
+
+fn sum_digits(digits: Vec<u32>) -> u32 {
+    digits.iter().sum()
+}
+
+fn is_divisable_by_10(num: u32) -> bool {
+    num % 10 == 0
+}
+
 pub fn is_valid(input: &str) -> bool {
-    let no_whitespace = input.replace(" ", "");
-
-    if no_whitespace.len() < 2 {
-        return false;
-    }
-
-    // no_whitespace.enumerate(|ix, char|)
-
-    for no_whitespace.iter().enumerate() {
-        
-    }
-
-    true
+    remove_whitespace(input)
+        .and_then(to_digits)
+        .map(double_second_digits)
+        .map(sum_digits)
+        .map(is_divisable_by_10)
+        .unwrap_or(false)
 }
--- rust/luhn/tests/luhn.rs
+++ rust/luhn/tests/luhn.rs
@@ -34,43 +34,36 @@ fn invalid_credit_card_is_invalid() {
 }
 
 #[test]
-#[ignore]
 fn strings_that_contain_non_digits_are_invalid() {
     assert!(!is_valid("046a 454 286"));
 }
 
 #[test]
-#[ignore]
 fn punctuation_is_invalid() {
     assert!(!is_valid("055-444-285"));
 }
 
 #[test]
-#[ignore]
 fn symbols_are_invalid() {
     assert!(!is_valid("055£ 444$ 285"));
 }
 
 #[test]
-#[ignore]
 fn single_digit_with_space_is_invalid() {
     assert!(!is_valid(" 0"));
 }
 
 #[test]
-#[ignore]
 fn lots_of_zeros_are_valid() {
     assert!(is_valid(" 00000"));
 }
 
 #[test]
-#[ignore]
 fn another_valid_sin() {
     assert!(is_valid("055 444 285"));
 }
 
 #[test]
-#[ignore]
 fn nine_doubled_is_nine() {
     assert!(is_valid("091"));
 }
