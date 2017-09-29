eb0d2211e939a4a274754a183b3081e1bd5cc29a
Sebastian Porto <s@porto5.com>
2017-09-29 20:39:20 +1000

    Add Run Length Encoding in Rust

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -1,56 +1,69 @@
-pub fn encode(input: String) -> String {
-
-    // fn group(acc: Vec<(char, usize)>, char: char) -> Vec<(char, usize)> {
-    //     match acc.last() {
-    //         Some(last) => {
-    //             let &(c, s) = last;
-    //             if c == char {
-
-    //                 acc.push((c, s + 1));
-    //                 acc
-    //             } else {
-    //                 acc.push((c, 1));
-    //                 acc
-    //             }
-    //         }
-    //         _ => vec![(char, 1)],
-    //     }
-    // }
-
-    fn encode_tuple(tuple: (char, usize)) -> String {
-        let (c, s) = tuple;
-        c.to_string() + &s.to_string()
-    }
-
-    let mut tuples = vec![];
-
-    for char in &input.chars() {
-        match tuples.last() {
-            Some(last) => {
-                let &(c, s) = last;
-                if c == char {
-                    tuples.pop();
-                    tuples.push((char, s + 1));
-                } else {
-                    tuples.push((char, 1));
-                }
+fn split_by_char(acc: Vec<(char, usize)>, c: char) -> Vec<(char, usize)> {
+    match acc.last() {
+        Some(last) => {
+            let mut acc2 = acc.clone();
+            let &(c2, s) = last;
+            if c == c2 {
+                acc2.pop();
+                acc2.push((c, s + 1));
+            } else {
+                acc2.push((c, 1));
             }
-            _ => tuples.push((char, 1)),
+            acc2
         }
+        None => vec![(c, 1)],
     }
+}
 
-    tuples.into_iter()
+fn encode_tuple(tuple: (char, usize)) -> String {
+    let (c, s) = tuple;
+    if s == 1 {
+        c.to_string()
+    } else {
+        s.to_string() + &c.to_string()
+    }
+}
+
+pub fn encode(input: &str) -> String {
+    input.chars()
+        .fold(vec![], split_by_char)
+        .into_iter()
         .map(encode_tuple)
         .collect::<Vec<String>>()
         .join("")
-    // input.chars()
-    //     .fold(vec![], group)
-    //     .into_iter()
-    //     .map(encode_tuple)
-    //     .collect::<Vec<String>>()
-    //     .join("")
 }
 
-pub fn decode(input: String) -> String {
-    "".to_string()
+fn split_by_num_and_char(acc: Vec<String>, c: char) -> Vec<String> {
+    match acc.last() {
+        Some(last) => {
+            let mut acc2 = acc.clone();
+            if c.is_digit(10) {
+                acc2.pop();
+                acc2.push(c.to_string() + last);
+            } else {
+                acc2.push(c.to_string());
+            }
+            acc2
+        }
+        None => vec![c.to_string()]
+    }
+}
+
+fn expand(s: String) -> String {
+    let (n, c) = s.split_at(s.len() - 1);
+    let n2 = n.parse::<usize>().unwrap_or(1);
+    c.repeat(n2).to_string()
+}
+
+pub fn decode(input: &str) -> String {
+    // 2A3B4C -> AABBBCCCC
+    input.chars()
+        .into_iter()
+        .rev()
+        .fold(vec![], split_by_num_and_char)
+        .into_iter()
+        .rev()
+        .map(expand)
+        .collect::<Vec<String>>()
+        .join("")
 }
--- rust/run-length-encoding/tests/run-length-encoding.rs
+++ rust/run-length-encoding/tests/run-length-encoding.rs
@@ -8,32 +8,27 @@ fn test_encode_empty_string() {
 }
 
 #[test]
-#[ignore]
 fn test_encode_single_characters() {
     assert_eq!("XYZ", rle::encode("XYZ"));
 }
 
 #[test]
-#[ignore]
 fn test_encode_string_with_no_single_characters() {
     assert_eq!("2A3B4C", rle::encode("AABBBCCCC"));
 }
 
 #[test]
-#[ignore]
 fn test_encode_single_characters_mixed_with_repeated_characters() {
     assert_eq!("12WB12W3B24WB", rle::encode(
         "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"));
 }
 
 #[test]
-#[ignore]
 fn test_encode_multiple_whitespace_mixed_in_string() {
     assert_eq!("2 hs2q q2w2 ", rle::encode("  hsqq qww  "));
 }
 
 #[test]
-#[ignore]
 fn test_encode_lowercase_characters() {
     assert_eq!("2a3b4c", rle::encode("aabbbcccc"));
 }
@@ -41,38 +36,32 @@ fn test_encode_lowercase_characters() {
 // decoding tests
 
 #[test]
-#[ignore]
 fn test_decode_empty_string() {
     assert_eq!("", rle::decode(""));
 }
 
 #[test]
-#[ignore]
 fn test_decode_single_characters_only() {
     assert_eq!("XYZ", rle::decode("XYZ"));
 }
 
 #[test]
-#[ignore]
 fn test_decode_string_with_no_single_characters() {
     assert_eq!("AABBBCCCC", rle::decode("2A3B4C"));
 }
 
 #[test]
-#[ignore]
 fn test_decode_single_characters_with_repeated_characters() {
     assert_eq!("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB",
                rle::decode("12WB12W3B24WB"));
 }
 
 #[test]
-#[ignore]
 fn test_decode_multiple_whitespace_mixed_in_string() {
     assert_eq!("  hsqq qww  ", rle::decode("2 hs2q q2w2 "));
 }
 
 #[test]
-#[ignore]
 fn test_decode_lower_case_string() {
     assert_eq!("aabbbcccc", rle::decode("2a3b4c"));
 }
@@ -80,7 +69,6 @@ fn test_decode_lower_case_string() {
 // consistency test
 
 #[test]
-#[ignore]
 fn test_consistency() {
     assert_eq!("zzz ZZ  zZ", rle::decode(rle::encode("zzz ZZ  zZ").as_str()));
 }
