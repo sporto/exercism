b49982ec1277d4c6a60652c1016b3f368a26f467
Sebastian Porto <s@porto5.com>
2017-10-01 17:54:49 +1100

    Clock in Rust

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,25 +1,31 @@
 use std::fmt;
 
+fn modulo(a: i16, b: i16) -> i16 {
+    (a % b + b) % b
+}
+
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    h: i8,
-    m: i8,
+    h: i16,
+    m: i16,
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:02}:{:02}", self.h, self.m)
-        // Ok(())
     }
 }
 
 impl Clock {
-    pub fn new(h: i8, m: i8) -> Clock {
+    pub fn new(h: i16, m: i16) -> Clock {
+        let mins = modulo(h * 60 + m, 24 * 60);
+        let h = mins / 60;
+        let m = mins % 60;
         Clock { h, m }
     }
 
-    pub fn add_minutes(self: Clock, mins: i8) -> Clock {
-        Clock { h : self.h, m : self.m + mins }
+    pub fn add_minutes(self: Clock, mins: i16) -> Clock {
+        Clock::new(self.h, self.m + mins)
     }
 }
 
