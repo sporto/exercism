16d4e338dd183ff3f342c0ec4c14a397daa09496
Sebastian <s@porto5.com>
2017-10-07 14:27:20 +1100

    Crypto

--- rust/crypto-square/src/lib.rs
+++ rust/crypto-square/src/lib.rs
@@ -1,20 +1,37 @@
+use std::ascii::AsciiExt;
+
+fn is_valid(c: &char) -> bool {
+    c.is_alphabetic() && c.is_ascii()
+}
+
 pub fn encrypt(input: &str) -> String {
+    if input == "" {
+        return "".to_string();
+    }
+
     let joined = input
         .to_lowercase()
         .chars()
-        .filter(|c| c.is_alphabetic())
+        .filter(is_valid)
         .collect::<Vec<char>>();
+
+    let len = joined.len() as f32;
     
     let joined2: String = joined.iter().cloned().collect();
 
-    let cols: usize = (joined.len() as f32)
-        .sqrt().ceil() as usize;
+    let cols: usize = len
+        .sqrt().floor() as usize;
+    
+    let rows = (len / cols as f32).ceil() as usize;
+
+    println!("len {} cols {} rows {}", len, cols, rows);
 
     let mut output = "".to_string();
 
-    for a in 0..cols {
+    for a in 0..rows {
         for b in 0..cols {
-            let pos = a + (b * cols);
+            let pos = a + (b * rows);
+            // println!("pos {}", pos);
             output = output + &joined2[pos..pos+1];
         };
         output = output + " ";
