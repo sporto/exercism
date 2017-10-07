d3fb15e4e4b58f5dd9483a6bf2b3374e6b4f6b14
Sebastian <s@porto5.com>
2017-10-07 12:43:32 +1100

    Use for loop

--- rust/crypto-square/src/lib.rs
+++ rust/crypto-square/src/lib.rs
@@ -4,18 +4,21 @@ pub fn encrypt(input: &str) -> String {
         .chars()
         .filter(|c| c.is_alphabetic())
         .collect::<Vec<char>>();
+    
+    let joined2: String = joined.iter().cloned().collect();
 
     let cols: usize = (joined.len() as f32)
         .sqrt().ceil() as usize;
 
     let mut output = "".to_string();
 
-    for chunk in joined.chunks(cols) {
-        for c in chunk {
-            output = output + &c.to_string();
+    for a in 0..cols {
+        for b in 0..cols {
+            let pos = a + (b * cols);
+            output = output + &joined2[pos..pos+1];
         };
         output = output + " ";
     };
 
-    output
+    output.trim().to_string()
 }
