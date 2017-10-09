2e53c54b79fa3087368bf2ed4048d96689337d6c
Sebastian <s@porto5.com>
2017-10-09 17:37:11 +1100

    Add first_and_last

--- rust/rotational-cipher/src/lib.rs
+++ rust/rotational-cipher/src/lib.rs
@@ -1,20 +1,17 @@
+fn first_and_last(c: char) -> (u8, u8) {
+    if c.is_lowercase() {
+        ('a' as u8, 'z' as u8)
+    } else {
+        ('A' as u8, 'Z' as u8)
+    }
+}
+
 fn rotate_char(offset: usize, c: char) -> char {
     if !c.is_alphabetic()  {
         return c
     };
 
-    let first = if c.is_lowercase() {
-        'a'
-    } else {
-        'A'
-    } as u8;
-
-    let last =  if c.is_lowercase() {
-        'z'
-    } else {
-        'Z'
-    } as u8;
-
+    let (first, last) = first_and_last(c);
     let range = last - first + 1;
     let code  = (c as u8 - first + offset as u8) % range + first;
     code as char
