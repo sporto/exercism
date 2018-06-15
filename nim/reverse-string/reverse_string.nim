05c470c9c2faee819acf10a2052dda338d37387d
Sebastian Porto <s@porto5.com>
2018-06-15 18:29:56 +1000

    Update reverse string

--- nim/reverse-string/reverse_string.nim
+++ nim/reverse-string/reverse_string.nim
@@ -1,12 +1,6 @@
-proc reduceR(source: string, f: proc(acc: string, v: char): string, acc: string): string =
-  result = acc
-  var i = high(source)
-  while i >= 0:
-    result = f(result, source[i])
-    dec i
-
-proc accumulate(acc: string, c: char): string =
-  acc & c
-
 proc reverse*(input: string): string =
-    input.reduceR(accumulate , "")
+  result = ""
+  var i = high(input)
+  while i >= 0:
+    result &= input[i]
+    dec i
