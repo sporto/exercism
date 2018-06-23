3f6424e500b212bdbed161c5389ba09ce7fb36a6
Sebastian Porto <s@porto5.com>
2018-06-23 13:44:02 +1000

    Update roman numerals

--- nim/roman-numerals/roman_numerals.nim
+++ nim/roman-numerals/roman_numerals.nim
@@ -1,64 +1,24 @@
-import math
+import sequtils
 
-proc digitForExponent(n: int, exponent: int): int =
-  let tens = (10 ^ exponent).float
-  fmod((n.float / tens).floor, 10.0).int
-
-proc charForExponent(exponent: int): string =
-  case exponent:
-    of 0:
-      "I"
-    of 1:
-      "X"
-    of 2:
-      "C"
-    of 3:
-      "M"
-    else:
-      ""
-
-proc halfwayCharForExponent(exponent: int): string =
-  case exponent:
-    of 0:
-      "V"
-    of 1:
-      "L"
-    of 2:
-      "D"
-    else:
-      ""
-
-proc numeralFor(n: int, exponent: int): string =
-  let i = charForExponent(exponent)
-  let v = halfwayCharForExponent(exponent)
-  let x = charForExponent(exponent + 1)
-
-  case n:
-    of 1:
-      i
-    of 2:
-      i & i
-    of 3:
-      i & i & i
-    of 4:
-      i & v
-    of 5:
-      v
-    of 6:
-      v & i
-    of 7:
-      v & i & i
-    of 8:
-      v & i & i & i
-    of 9:
-      i & x
-    else:
-      ""
+proc nextNumeral(n: int): (int, string) =
+  [
+    (1000, "M"),
+    (900, "CM"),
+    (500, "D"),
+    (400, "CD"),
+    (100, "C"),
+    (90, "XC"),
+    (50, "L"),
+    (40, "XL"),
+    (10, "X"),
+    (9, "IX"),
+    (5, "V"),
+    (4, "IV"),
+    (1, "I"),
+    (0, ""),
+  ].filterIt(n >= it[0])[0]
 
 proc numeral*(n: int): string =
-  result = ""
-  var exponent = 6
-  while exponent >= 0:
-    let digit = digitForExponent(n, exponent)
-    result &= numeralFor(digit, exponent)
-    dec exponent
+  if n == 0: return ""
+  let next = nextNumeral(n)
+  next[1] & numeral(n - next[0])
