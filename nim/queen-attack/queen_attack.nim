7a64465c05583d1658ff2c724fd17e235c720bfe
Sebastian Porto <s@porto5.com>
2018-06-25 23:53:14 +1000

    Update canAttack

--- nim/queen-attack/queen_attack.nim
+++ nim/queen-attack/queen_attack.nim
@@ -3,30 +3,28 @@ import sequtils
 type
   Pos = tuple[rank: int, file: int]
 
-proc isValidPos(a: Pos): bool =
-  a[0] >= 0 and a[0] <= 7 and a[1] >= 0 and a[1] <= 7
+proc isInside(a: Pos): bool =
+  let (x, y) = a
+  x >= 0 and x <= 7 and y >= 0 and y <= 7
+
 
 proc validate(w: Pos, b: Pos) =
   if w == b:
     raise newException(ValueError, "Same position")
-  
-  if not isValidPos(w) or not isValidPos(b):
+
+  if not isInside(w) or not isInside(b):
     raise newException(ValueError, "Invalid position")
 
 
 proc canAttack*(w: Pos, b: Pos): bool =
   validate(w, b)
 
-  if w[0] == b[0]:
-    return true
+  let rank = w[0] == b[0]
+  let file = w[1] == b[1]
+  let diagonal = abs(w[0] - b[0]) == abs(w[1] - b[1])
 
-  if w[1] == b[1]:
-    return true
+  rank or file or diagonal
 
-  if abs(w[0] - b[0]) == abs(w[1] - b[1]):
-    return true
-
-  false
 
 proc charFor(w: Pos, b: Pos, current: Pos): char =
   if w == current:
@@ -35,6 +33,7 @@ proc charFor(w: Pos, b: Pos, current: Pos): char =
     'B'
   else:
     '_' 
+
 
 proc board*(w: Pos, b: Pos): seq[string] =
   validate(w, b)
