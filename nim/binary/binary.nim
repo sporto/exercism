74b1cd2bdc5741df4645fd544e5d583bda8d2794
Sebastian Porto <s@porto5.com>
2018-06-26 20:21:44 +1000

    Update binary

--- nim/binary/binary.nim
+++ nim/binary/binary.nim
@@ -1,15 +1,13 @@
 import strutils
 
-proc bit(c: char): int =
+proc charToBit(c: char): int =
   result = parseInt(c & "")
   if result > 1:
     raise newException(ValueError, "Not binary")
 
 proc binary*(input: string): int =
-  result = 0
-  let len = input.len - 1
-  var exp = len
-  while exp >= 0:
-    let bit = bit(input[len - exp])
+  var exp = 0
+  while exp < input.len:
+    let bit = charToBit(input[input.len - 1 - exp])
     result += (1 shl exp) * bit
-    dec exp
+    inc exp
