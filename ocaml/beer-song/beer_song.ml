5134a48a3ea781723fa210b0e93659bbde9de182
Sebastian Porto <s@porto5.com>
2021-09-16 19:52:08 +1000

    Add OCaml beer song

---
+++ ocaml/beer-song/beer_song.ml
@@ -0,0 +1,22 @@
+
+let debug_print s =
+    Printf.printf "%s" s; s
+
+let verse =
+    function
+    | 0 -> "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+    | 1 -> "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall."
+    | 2 -> "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall."
+    | n -> Printf.sprintf "%i bottles of beer on the wall, %i bottles of beer.\nTake one down and pass it around, %i bottles of beer on the wall." n n (n - 1)
+
+
+let rec recite_do from until acc =
+    if until > 0 then
+        recite_do (from - 1) (until - 1) (verse from :: acc)
+    else
+        acc
+
+let recite from until =
+    recite_do from until []
+    |> List.rev
+    |> Base.String.concat ~sep:"\n\n"
---
+++ ocaml/beer-song/beer_song.mli
@@ -0,0 +1,4 @@
+(** Beer-song exercise *)
+
+(* Starting from the first parameter, recite n verses where n is the second parameter *)
+val recite : int -> int -> string
