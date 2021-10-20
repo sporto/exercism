fda027b2d8105842e6fc33e06ba9055d0d226b08
Sebastian <s@porto5.com>
2021-10-20 15:55:29 +1100

    Add F# AnnalynsInfiltration

---
+++ fsharp/annalyns-infiltration/AnnalynsInfiltration.fs
@@ -0,0 +1,16 @@
+module AnnalynsInfiltration
+
+let canFastAttack (knightIsAwake: bool) : bool = not knightIsAwake
+
+let canSpy (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool) : bool =
+    knightIsAwake || archerIsAwake || prisonerIsAwake
+
+let canSignalPrisoner (archerIsAwake: bool) (prisonerIsAwake: bool) : bool = prisonerIsAwake && not archerIsAwake
+
+let canFreePrisoner (knightIsAwake: bool) (archerIsAwake: bool) (prisonerIsAwake: bool) (petDogIsPresent: bool) : bool =
+    if petDogIsPresent then
+        not archerIsAwake
+    else
+        prisonerIsAwake
+        && not archerIsAwake
+        && not knightIsAwake
