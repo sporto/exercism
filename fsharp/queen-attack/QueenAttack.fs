7ec133065681f1c4717bd24f483f83e7492b3e30
Sebastian Porto <s@porto5.com>
2021-11-01 22:04:10 +1100

    Add F# QueenAttack

---
+++ fsharp/queen-attack/QueenAttack.fs
@@ -0,0 +1,7 @@
+﻿module QueenAttack
+
+let create ((r, c): int * int) =
+       r > 0 && r < 8 && c > 0 && c < 8
+
+let canAttack ((r1, c1): int * int) ((r2, c2): int * int) =
+    r1 = r2 || c1 = c2 || (r1 - c1) = (r2 - c2) || (r1 + c1) = (r2 + c2)
