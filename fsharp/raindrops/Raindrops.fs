30e03be349e36f1d5fa1d1913b0259664195de43
Sebastian Porto <s@porto5.com>
2021-11-04 16:33:39 +1100

    Add F# Raindrops

---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,12 @@
+﻿module Raindrops
+
+let add word factor n =
+    if n % factor = 0 then word else ""
+
+let convert (number: int): string =
+    let
+        phrase =
+            add "Pling" 3 number ^ add "Plang" 5 number ^ add "Plong" 7 number
+    match phrase with
+    | "" -> number.ToString()
+    | _ -> phrase
