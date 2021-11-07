8047970a011374038ef8582a8ed1451ee62441b0
Sebastian Porto <s@porto5.com>
2021-11-07 11:30:16 +1100

    Add F# GradeSchool

---
+++ fsharp/grade-school/GradeSchool.fs
@@ -0,0 +1,26 @@
+﻿module GradeSchool
+
+type School = Map<int, string list>
+
+let empty: School = Map.empty
+
+let add (student: string) (grade: int) (school: School): School =
+    Map.change grade (fun maybe_list ->
+        match maybe_list with
+        | None -> Some [student]
+        | Some list -> Some (student :: list)
+    ) school
+
+let roster (school: School): string list =
+    school
+    |> Map.toList
+    |> List.sortBy fst
+    |> List.map (snd >> List.sort)
+    |> List.concat
+
+let grade (number: int) (school: School): string list =
+    school
+    |> Map.toList
+    |> List.filter (fun (g, _) -> g = number)
+    |> List.map snd
+    |> List.concat
