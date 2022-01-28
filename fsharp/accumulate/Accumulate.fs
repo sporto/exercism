474719d2d7d99bec1eda38c984d073fd3dddfdba
Sebastian <s@porto5.com>
2022-01-28 16:00:19 +1100

    F# Accumulate

---
+++ fsharp/accumulate/Accumulate.fs
@@ -0,0 +1,12 @@
+﻿module Accumulate
+
+let rec accumulate_ func input acc =
+    match input with
+    | [] -> acc
+    | first::rest ->
+        accumulate_ func rest (func first :: acc)
+
+
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list =
+    accumulate_ func input []
+    |> List.rev
