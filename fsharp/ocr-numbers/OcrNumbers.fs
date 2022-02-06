f6e4c9649b382f9e5ee907c6f9438c5121a73295
Sebastian Porto <s@porto5.com>
2022-02-06 15:04:52 +1100

    F# OCR Numbers

--- fsharp/ocr-numbers/OcrNumbers.fs
+++ fsharp/ocr-numbers/OcrNumbers.fs
@@ -1,17 +1,84 @@
 module OcrNumbers
 
-let parse_char input = Some ""
+let n0 = [ " _ "; "| |"; "|_|"; "   " ]
 
-let break_chars line = List.chunkBySize 3 line
+let n1 = [ "   "; "  |"; "  |"; "   " ]
 
-let parse_line input =
-    input |> break_chars |> List.map parse_char
+let n2 = [ " _ "; " _|"; "|_ "; "   " ]
 
-let break_lines input = List.chunkBySize 4 input
+let n3 = [ " _ "; " _|"; " _|"; "   " ]
 
-let parse_lines input =
-    input |> break_lines |> List.map parse_line
+let n4 = [ "   "; "|_|"; "  |"; "   " ]
 
-let join_lines lines = lines |> List.map join_chars
+let n5 = [ " _ "; "|_ "; " _|"; "   " ]
 
-let convert input = input |> parse_lines |> join_lines
+let n6 = [ " _ "; "|_ "; "|_|"; "   " ]
+
+let n7 = [ " _ "; "  |"; "  |"; "   " ]
+
+let n8 = [ " _ "; "|_|"; "|_|"; "   " ]
+
+let n9 = [ " _ "; "|_|"; " _|"; "   " ]
+
+let ns =
+    [
+        n0
+        n1
+        n2
+        n3
+        n4
+        n5
+        n6
+        n7
+        n8
+        n9
+    ]
+
+let break_lines = List.chunkBySize 4
+
+let break_line_in_chars (line: string) : string list =
+    line
+    |> Seq.toList
+    |> List.chunkBySize 3
+    |> List.map (System.String.Concat)
+
+let break_chars (line: string list) : string list list =
+    line
+    |> List.map break_line_in_chars
+    |> List.transpose
+
+let is_valid_char char =
+    List.length char = 4
+    && (List.head char |> String.length = 3)
+
+let parse_char char =
+    // A char must have 4 rows
+    if is_valid_char char then
+        ns
+        |> List.indexed
+        |> List.tryPick (fun (ix, n) ->
+            if n = char then
+                Some(string ix)
+            else
+                None)
+        |> Option.orElse (Some "?")
+    else
+        None
+
+let all l =
+    if List.contains None l then
+        None
+    else
+        Some(List.map Option.get l)
+
+let convert (input: string list) =
+    input
+    |> break_lines
+    |> List.map break_chars
+    |> List.map (
+        List.map parse_char
+        >> all
+        >> Option.map (String.concat "")
+    )
+    |> all
+    |> Option.map (String.concat ",")
--- fsharp/ocr-numbers/OcrNumbersTests.fs
+++ fsharp/ocr-numbers/OcrNumbersTests.fs
@@ -7,161 +7,127 @@ open OcrNumbers
 
 [<Fact>]
 let ``Recognizes 0`` () =
-    let rows = 
-        [ " _ ";
-          "| |";
-          "|_|";
-          "   " ]
+    let rows = [ " _ "; "| |"; "|_|"; "   " ]
     convert rows |> should equal (Some "0")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Recognizes 1`` () =
-    let rows = 
-        [ "   ";
-          "  |";
-          "  |";
-          "   " ]
+    let rows = [ "   "; "  |"; "  |"; "   " ]
     convert rows |> should equal (Some "1")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Unreadable but correctly sized inputs return ?`` () =
-    let rows = 
-        [ "   ";
-          "  _";
-          "  |";
-          "   " ]
+    let rows = [ "   "; "  _"; "  |"; "   " ]
     convert rows |> should equal (Some "?")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``Input with a number of lines that is not a multiple of four raises an error`` () =
-    let rows = 
-        [ " _ ";
-          "| |";
-          "   " ]
+[<Fact>]
+let ``Input with a number of lines that is not a multiple of four raises an error``
+    ()
+    =
+    let rows = [ " _ "; "| |"; "   " ]
     convert rows |> should equal None
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``Input with a number of columns that is not a multiple of three raises an error`` () =
-    let rows = 
-        [ "    ";
-          "   |";
-          "   |";
-          "    " ]
+[<Fact>]
+let ``Input with a number of columns that is not a multiple of three raises an error``
+    ()
+    =
+    let rows = [ "    "; "   |"; "   |"; "    " ]
     convert rows |> should equal None
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Recognizes 110101100`` () =
-    let rows = 
-        [ "       _     _        _  _ ";
-          "  |  || |  || |  |  || || |";
-          "  |  ||_|  ||_|  |  ||_||_|";
-          "                           " ]
+    let rows =
+        [
+            "       _     _        _  _ "
+            "  |  || |  || |  |  || || |"
+            "  |  ||_|  ||_|  |  ||_||_|"
+            "                           "
+        ]
+
     convert rows |> should equal (Some "110101100")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Garbled numbers in a string are replaced with ?`` () =
-    let rows = 
-        [ "       _     _           _ ";
-          "  |  || |  || |     || || |";
-          "  |  | _|  ||_|  |  ||_||_|";
-          "                           " ]
+    let rows =
+        [
+            "       _     _           _ "
+            "  |  || |  || |     || || |"
+            "  |  | _|  ||_|  |  ||_||_|"
+            "                           "
+        ]
+
     convert rows |> should equal (Some "11?10?1?0")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Recognizes 2`` () =
-    let rows = 
-        [ " _ ";
-          " _|";
-          "|_ ";
-          "   " ]
+    let rows = [ " _ "; " _|"; "|_ "; "   " ]
     convert rows |> should equal (Some "2")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Recognizes 3`` () =
-    let rows = 
-        [ " _ ";
-          " _|";
-          " _|";
-          "   " ]
+    let rows = [ " _ "; " _|"; " _|"; "   " ]
     convert rows |> should equal (Some "3")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Recognizes 4`` () =
-    let rows = 
-        [ "   ";
-          "|_|";
-          "  |";
-          "   " ]
+    let rows = [ "   "; "|_|"; "  |"; "   " ]
     convert rows |> should equal (Some "4")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Recognizes 5`` () =
-    let rows = 
-        [ " _ ";
-          "|_ ";
-          " _|";
-          "   " ]
+    let rows = [ " _ "; "|_ "; " _|"; "   " ]
     convert rows |> should equal (Some "5")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Recognizes 6`` () =
-    let rows = 
-        [ " _ ";
-          "|_ ";
-          "|_|";
-          "   " ]
+    let rows = [ " _ "; "|_ "; "|_|"; "   " ]
     convert rows |> should equal (Some "6")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Recognizes 7`` () =
-    let rows = 
-        [ " _ ";
-          "  |";
-          "  |";
-          "   " ]
+    let rows = [ " _ "; "  |"; "  |"; "   " ]
     convert rows |> should equal (Some "7")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Recognizes 8`` () =
-    let rows = 
-        [ " _ ";
-          "|_|";
-          "|_|";
-          "   " ]
+    let rows = [ " _ "; "|_|"; "|_|"; "   " ]
     convert rows |> should equal (Some "8")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Recognizes 9`` () =
-    let rows = 
-        [ " _ ";
-          "|_|";
-          " _|";
-          "   " ]
+    let rows = [ " _ "; "|_|"; " _|"; "   " ]
     convert rows |> should equal (Some "9")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
+[<Fact>]
 let ``Recognizes string of decimal numbers`` () =
-    let rows = 
-        [ "    _  _     _  _  _  _  _  _ ";
-          "  | _| _||_||_ |_   ||_||_|| |";
-          "  ||_  _|  | _||_|  ||_| _||_|";
-          "                              " ]
+    let rows =
+        [
+            "    _  _     _  _  _  _  _  _ "
+            "  | _| _||_||_ |_   ||_||_|| |"
+            "  ||_  _|  | _||_|  ||_| _||_|"
+            "                              "
+        ]
+
     convert rows |> should equal (Some "1234567890")
 
-[<Fact(Skip = "Remove this Skip property to run this test")>]
-let ``Numbers separated by empty lines are recognized. Lines are joined by commas.`` () =
-    let rows = 
-        [ "    _  _ ";
-          "  | _| _|";
-          "  ||_  _|";
-          "         ";
-          "    _  _ ";
-          "|_||_ |_ ";
-          "  | _||_|";
-          "         ";
-          " _  _  _ ";
-          "  ||_||_|";
-          "  ||_| _|";
-          "         " ]
-    convert rows |> should equal (Some "123,456,789")
+[<Fact>]
+let ``Numbers separated by empty lines are recognized. Lines are joined by commas.``
+    ()
+    =
+    let rows =
+        [
+            "    _  _ "
+            "  | _| _|"
+            "  ||_  _|"
+            "         "
+            "    _  _ "
+            "|_||_ |_ "
+            "  | _||_|"
+            "         "
+            " _  _  _ "
+            "  ||_||_|"
+            "  ||_| _|"
+            "         "
+        ]
 
+    convert rows |> should equal (Some "123,456,789")
